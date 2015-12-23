cd8c2dc81ed02530b40be45204dbf228023c23e3
Vincent Moisan <vincent.moisan@gmail.com>
2015-12-23 16:47:27 +0100

    each

--- mdwk_lobster/script.js
+++ mdwk_lobster/script.js
@@ -1,8 +1,9 @@
 $('.artist').click(function () {
-    if($(this).hasClass("active")){
-        $(this).removeClass("active"); 
-    }else{
+    $('.artist').each(function() {
+    $(this).removeClass("active"); 
+    });
        $(this).addClass("active"); 
-    }
+    
     
 });
+
