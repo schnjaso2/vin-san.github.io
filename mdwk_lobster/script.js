a03703019c50f45f2e5edf52460cbf84d3ec6e8b
Vincent Moisan <vincent.moisan@gmail.com>
2015-12-23 17:17:03 +0100

    all track

--- mdwk_lobster/script.js
+++ mdwk_lobster/script.js
@@ -1,17 +1,31 @@
+var audioElement = document.createElement('audio');
+
 $('.artist').click(function () {
-    
 
-    
-    if($(this).hasClass("active")){
-        $(this).removeClass("active"); 
-    }else{
-            $('.artist').each(function() {
-    $(this).removeClass("active"); 
-    });
-        
-       $(this).addClass("active"); 
-    }
-    
+     audioElement.pause();
+
+
+             if ($(this).hasClass("active")) {
+                 
+                 $(this).removeClass("active");
+     
+             } else {
+                 
+                 
+                 $('.artist').each(function () {
+                     $(this).removeClass("active");
+                 });
+
+            
+
+                 $(this).addClass("active");
+
+                 var track = this.getAttribute("track");
+
+                 audioElement.setAttribute('src', 'mp3/' + track);
+
+                 audioElement.play();
+               
+             }
+
 });
-
-
