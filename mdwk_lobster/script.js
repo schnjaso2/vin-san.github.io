401d6ddadc38010ba0ba71094307b77a29f953ee
Vincent Moisan <vincent.moisan@gmail.com>
2015-12-23 17:32:40 +0100

    new turntable

--- mdwk_lobster/script.js
+++ mdwk_lobster/script.js
@@ -2,30 +2,19 @@ var audioElement = document.createElement('audio');
 
 $('.artist').click(function () {
 
-     audioElement.pause();
+    audioElement.pause();
 
+    if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+    } else {
 
-             if ($(this).hasClass("active")) {
-                 
-                 $(this).removeClass("active");
-     
-             } else {
-                 
-                 
-                 $('.artist').each(function () {
-                     $(this).removeClass("active");
-                 });
+        $('.artist').each(function () {
+            $(this).removeClass("active");
+        });
 
-            
-
-                 $(this).addClass("active");
-
-                 var track = this.getAttribute("track");
-
-                 audioElement.setAttribute('src', 'mp3/' + track);
-
-                 audioElement.play();
-               
-             }
-
+        $(this).addClass("active");
+        var track = this.getAttribute("track");
+        audioElement.setAttribute('src', 'mp3/' + track);
+        audioElement.play();
+    }
 });
